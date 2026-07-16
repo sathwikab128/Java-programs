5aca5098715a669885ec821ccc5f913afe303891
sathwikab128 <sathwika.b128@gmail.com>
2026-07-17 02:36:39 +0530

    Added ArmstrongNumber.java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,16 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int n = 153, sum = 0, temp = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        if (sum == temp)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
