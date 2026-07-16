0c08b0b4e2792ffab7cf1a9aff18ea2eeb237dc6
sathwikab128 <sathwika.b128@gmail.com>
2026-07-17 02:30:20 +0530

    Added LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,9 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 30;
+        if (a > b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
