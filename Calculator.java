8fafa22be886aff5dc823f9b3573bbdfe58fe927
sathwikab128 <sathwika.b128@gmail.com>
2026-07-17 02:42:18 +0530

    Added Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
