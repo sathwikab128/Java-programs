e033f6874f7707e536407e1cf71730b79da58aa4
sathwikab128 <sathwika.b128@gmail.com>
2026-07-17 02:41:13 +0530

    Added VowelOrConsonant.java

---
+++ VowelOrConsonant.java
@@ -0,0 +1,11 @@
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
+    }
+}
