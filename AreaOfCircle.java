6bb1a9ae72436e5e7f4f245e3a18dcc51b23294f
sathwikab128 <sathwika.b128@gmail.com>
2026-07-17 02:40:07 +0530

    Added AreaOfCircle.java

---
+++ AreaOfCircle.java
@@ -0,0 +1,8 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = 3.14 * radius * radius;
+
+        System.out.println("Area = " + area);
+    }
+}
